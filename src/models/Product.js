5d8e26071e4cddaa16218c66a1ea4d40ba74cbce
Torben Jost <torbenjost@proton.me>
2024-12-12 14:55:11 +0100

    refactor Product model to generate objects via function

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,33 +1,5 @@
-class Product {
-  constructor(productId, name, price) {
-    this.productId = productId;
-    this.name = name;
-    this.price = price;
-  }
-
-  get productId() {
-    return this.productId;
-  }
-
-  get productName() {
-    return this.name;
-  }
-
-  get productPrice() {
-    return this.productPrice;
-  }
-
-  set productId(value) {
-    this.productId = value;
-  }
-
-  set productName(value) {
-    this.name = value;
-  }
-
-  set productPrice(value) {
-    this.productPrice = value;
-  }
-
-  // no built-in hashCode method like in Java in Javascript
+function createProduct(productId, name, price) {
+  return { productId, name, price };
 }
+
+module.exports = { createProduct };
